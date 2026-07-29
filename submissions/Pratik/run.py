2b214bd94ed2a0b06b7fa27a2fd2934ee1daf76e
Pratik Devalatkar <pdevalatkar@withum.com>
2026-07-29 14:55:59 +0530

    Add queries and screenshots for beginner

---
+++ submissions/Pratik/run.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import duckdb
+
+con = duckdb.connect("training.duckdb")
+con.execute(Path("../../00_setup.sql").read_text())
+
+# run your query
+# con.sql(Path("sql/my_query.sql").read_text()).show()
+
+queries=Path("01_beginner.sql").read_text()
+ 
+for query in queries.split(";"):
+    query=query.strip()
+    if query:
+        print("\n==================================================")
+        print(query)
+        try:
+            con.sql(query).show()
+        except Exception as e:
+            print("Error",e)
